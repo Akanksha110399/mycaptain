f3d96a98d2f0fc67013ad7711fdaadd018da00f2
Akanksha110399 <61195841+Akanksha110399@users.noreply.github.com>
2020-12-19 09:58:26 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double num1,num2,prdct;
+    cout << "Enter two numbers: ";
+
+    cin >> num1 >> num2;
+
+    prdct = num1 * num2;
+
+    cout << "Product = " << prdct;
+
+    return 0;
+}
